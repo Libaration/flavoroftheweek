aa5a44fb7190a0368c99e6ca412922e440248fab
Cristian Deleon <libaration@gmail.com>
2020-12-11 17:19:29 -0500

    Grab from API for dynamic background on Song show page

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.6.1'
 gem 'bcrypt'
 gem 'dotenv-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'httparty'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.4'
 # Use sqlite3 as the database for Active Record
 gem 'rspotify'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def grab_background(artist)
+
+    request = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIFKEY']}&q=#{artist}&limit=5&offset=0&rating=g&lang=en").to_json
+    response = JSON.parse(request)["data"]
+
+    bgvid = nil
+    response.each do |gif|
+      break if bgvid.present?
+      bgvid = gif["images"]["hd"]["mp4"] if gif["images"]["hd"].present?
+    end
+    bgvid
+  end
 end
