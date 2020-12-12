ea1e5e12c778b92896ee8e2c8d4be8775cd955c1
Cristian Deleon <libaration@gmail.com>
2020-12-12 13:30:53 -0500

    Style tweaks for songs view and static home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,3 @@
 module ApplicationHelper
 
-  def grab_background(song)
-
-    request = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIFKEY']}&q=#{song.artist}&limit=5&offset=0&rating=g&lang=en").to_json
-    response = JSON.parse(request)["data"]
-
-    bgvid = nil
-    response.each do |gif|
-      break if bgvid.present?
-      bgvid = gif["images"]["hd"]["mp4"] if gif["images"]["hd"].present?
-    end
-    bgvid
-  end
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,6 +2,18 @@ module SongsHelper
   def dominantcolor(song)
     Miro.options[:color_count] = 3
     colors = Miro::DominantColors.new(song)
-    colors.to_hex.last
+    colors.to_hex
+  end
+  def grab_background(song)
+
+    request = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIFKEY']}&q=#{song.artist}&limit=5&offset=0&rating=g&lang=en").to_json
+    response = JSON.parse(request)["data"]
+
+    bgvid = nil
+    response.each do |gif|
+      break if bgvid.present?
+      bgvid = gif["images"]["hd"]["mp4"] if gif["images"]["hd"].present?
+    end
+    bgvid
   end
 end
