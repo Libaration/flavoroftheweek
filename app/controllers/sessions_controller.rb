47e46f314ed23579ee2adf8dd8762e976cba6a0a
Cristian Deleon <libaration@gmail.com>
2020-12-13 00:30:20 -0500

    adds login/session setting functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
     @user = User.new
   end
   def create
-
+    @user = User.where('email IS ?', params[:user][:email]).first
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      #redirect when i get around to making the model.
+    elsif !@user
+      redirect_to login_path, alert: "User not found"
+    else
+      redirect_to login_path, alert: "Incorrect password"
+    end
   end
 end
