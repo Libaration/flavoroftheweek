7e605ab033ae2aa6478e5f6782984a5d3fbb6356
Cristian Deleon <libaration@gmail.com>
2020-12-12 23:16:55 -0500

    adds spotify icon asset, builds login page skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root 'static#home'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 end
