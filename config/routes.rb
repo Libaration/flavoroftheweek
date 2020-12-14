68f67ef2feecf5bc40296b4115777dfe7a297cd1
Cristian Deleon <libaration@gmail.com>
2020-12-14 00:04:02 -0500

    Adds MVC skeleton for Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :songs, only: [:show, :create, :new, :destroy]
+  resources :posts
   root 'static#home'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_183826) do
+ActiveRecord::Schema.define(version: 2020_12_14_045747) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.integer "song_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "artist"
