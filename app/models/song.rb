a45b740ebb5a552b3fbf23f5178e3231a45d4774
Cristian Deleon <libaration@gmail.com>
2020-12-14 23:04:53 -0500

    Sets up associations for posts/songs/users

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ApplicationRecord
-
+  has_many :posts
+  has_many :users, through: :posts
   def self.find_or_create_by_spotify(song)
     where(:url => song.preview_url).first_or_create do |s|
       s.artist = song.artists.first.name
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :songs, through: :posts
   has_secure_password
-  has_many :songs, through: :user_songs
   validates :email, uniqueness: true
   validates_confirmation_of :password
 
