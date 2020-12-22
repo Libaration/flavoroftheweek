5b435993fb6c635b8ae4a5b3f9073f08747572b2
Cristian Deleon <libaration@gmail.com>
2020-12-22 05:34:47 -0500

    Fixes association bug renames comments columns

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,5 @@ class CommentsController < ApplicationController
   layout "posts"
   def index
     @post = Post.find(params[:post_id])
-    @song = @post.song
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :song
   belongs_to :original_post, class_name: 'Post', foreign_key: 'original_id', optional: true
-  has_many :post_comments
-  has_many :comments, through: :post_comments, source: :post
+  has_many :comments, class_name: "PostComment", foreign_key: :post_id
 
   def self.create_from_repost(post, currentuser)
     create(song: post.song, user: currentuser, content: post.content, original_post: post)
