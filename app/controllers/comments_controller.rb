b0323972f90f3557ba96020b2270341f5317d3f5
Cristian Deleon <libaration@gmail.com>
2020-12-22 06:21:04 -0500

    Allows creation of new comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,18 @@ class CommentsController < ApplicationController
   def index
     @post = Post.find(params[:post_id])
   end
+  def create
+    @post = Post.find(params[:post_id])
+    @post.comments.create(comment_params.merge(user: current_user))
+    redirect_to post_comments_path(@post)
+  end
+
+  def comment_params
+    params.require(:post_comment).permit(:message, :post_id, :user_id)
+  end
+
+  private
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
